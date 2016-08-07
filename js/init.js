a2745021b32cde1062cdc6e5f409accf5fad1e04
Mateusnunes <mateununes@me.com>
2016-08-07 00:21:32 -0300

    SIDE BAR UHULLLLL

--- js/init.js
+++ js/init.js
@@ -1,17 +1,18 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $(".button-collapse").sideNav();
+        
+    $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$(document).ready(function(){
-      $('.parallax').parallax();
-});
 
 $('ul.tabs a').on('click', function(e){
   if($(this).attr("target") ) {
     window.location = $(this).attr("href");
   }
 });
+
+$(".button-collapse").sideNav();
